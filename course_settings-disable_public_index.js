dbf41ac94399ae89fdccbd7a931b6fa28bdea666
cmcasey79 <51375759+cmcasey79@users.noreply.github.com>
2019-06-04 11:56:47 -0400

    Update course_settings-disable_public_index.js

--- course_settings-disable_public_index.js
+++ course_settings-disable_public_index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	// Checks if page is course settings
 	if (/^\/courses\/[0-9]+\/settings$/.test(window.location.pathname)) {
-		// Checks that current user role is not an admin to hide term start end date and visibility options. Admins can access all settings.
+		// Checks that current user role is not an admin. Admins can access all settings.
 		if($.inArray('admin',ENV.current_user_roles) == -1){
 			// Disables the 'Include this course in the public course index' checkbox
 			$('#course_indexed').prop('disabled',true);	
