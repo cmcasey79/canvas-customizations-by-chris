4f904ee4577131826961c111b8aa7b066c5eb855
cmcasey79 <51375759+cmcasey79@users.noreply.github.com>
2019-06-04 11:57:25 -0400

    Update course_settings-disable_visibility_options.js

--- course_settings-disable_visibility_options.js
+++ course_settings-disable_visibility_options.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 
 	// Checks if page is course settings
 	if (/^\/courses\/[0-9]+\/settings$/.test(window.location.pathname)) {
-		// Checks that current user role is not an admin to hide term start end date and visibility options. Admins can access all settings.
+		// Checks that current user role is not an admin. Admins can access all settings.
 		if($.inArray('admin',ENV.current_user_roles) == -1){
 			// Disables the 'Visibility' dropdown
 			$('#course_course_visibility').prop('disabled',true);	
 			// Disables the 'Visibility' customization checkbox
 			$('#course_custom_course_visibility').prop('disabled',true);
+			// Disables the 'Syllabus' customization checkbox
+			$('#Syllabus').prop('disabled',true);	
 		}
 	}
 
