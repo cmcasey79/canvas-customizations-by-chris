b26da8726ee3439f1b2f507b1b31dfb18756aaef
cmcasey79 <51375759+cmcasey79@users.noreply.github.com>
2021-05-20 10:09:52 -0400

    Update user_profile-disable_default_email_change.js

--- user_profile-disable_default_email_change.js
+++ user_profile-disable_default_email_change.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
 	// Check if page is user profile
 	if ((/^\/profile$/.test(window.location.pathname)) || (/^\/profile\/settings$/.test(window.location.pathname))) {
-		// Checks that current user role is not an admin to hide default email change icon. Admins can access all settings.
-		if($.inArray('admin',ENV.current_user_roles) == -1){
-			//Hide links to change default email
-			$('.email_meta:gt(0)').find('a').hide();
+		//Hide links to change default email
+		const links=document.querySelectorAll('.email_meta > a');
+		for (var i = 1; i < links.length; ++i) {
+			links[i].style.display='none';
 		}
 	}
 
