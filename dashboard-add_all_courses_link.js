59d8949786250d42efe42cf7f7e836ff9e6d620d
cmcasey79 <51375759+cmcasey79@users.noreply.github.com>
2023-08-17 15:59:18 -0400

    Update dashboard-add_all_courses_link.js
    Remove jQuery elements and replace with pure JavaScript

--- dashboard-add_all_courses_link.js
+++ dashboard-add_all_courses_link.js
@@ -1,9 +1,13 @@
 // Adds a link to All Courses directly ont he dashboard.  Helps users find future or concluded courses and reduced amount of tickets about "missing" courses at the end of a term.
-$(document).ready(function(){
-
+var do_customizations = function(){
 	// Checks if page is dashboard
 	if (window.location.pathname=='/') {
-		$('#DashboardCard_Container').before('<div><p>Additional courses, including those from previous semesters, may be found on your <a href="/courses">All Courses</a> page.</p></div>');
+		document.getElementById('DashboardCard_Container').insertAdjacentHTML('beforebegin', ''<div><p>Additional courses, including those from previous semesters, may be found on your <a href="/courses">All Courses</a> page.</p></div>');
 	}
+}
 
-});
+if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+	do_customizations();
+} else {
+	document.addEventListener("DOMContentLoaded", do_customizations);
+}
